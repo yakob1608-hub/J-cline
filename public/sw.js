8dd5f491bba677249550ca1e24ee61dd16a776da
Jacob <jacob@example.com>
2026-04-15 14:35:40 -0700

    add monetag ad script

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10880188
+    "zoneId": 10880896
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
